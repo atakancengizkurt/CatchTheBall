dc257ea0e62fb6c70a325dc9b3495172a52e207f
Atakan Cengiz KURT <atakan.kurt@loodos.com>
2021-10-13 20:38:14 +0300

    Initial Commit

---
+++ CatchTheBall/CatchTheBallApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatchTheBallApp.swift
+//  CatchTheBall
+//
+//  Created by Atakan Cengiz KURT on 13.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct CatchTheBallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
